bb80bc0b987f65e9217aef0bf88e03b1896598ec
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-01-23 18:00:39 +0530

    created the lab 3

---
+++ Recursion_3.java
@@ -0,0 +1,79 @@
+// import java.util.ArrayList;
+// import java.util.List;
+
+// public class Recursion_3 {
+//     public static void main(String[] args) {
+//         List<String> list=new ArrayList<>();
+//         String s="aba";
+//         uniquePermutation(s,"",list);
+//         System.out.println(list);
+//     }
+//     private static void uniquePermutation(String s,String ans,List<String> list){
+//         if(s.length()==0){
+//             if(list.contains(ans)){
+
+//             }
+//             else{
+//                 list.add(ans);
+//             }
+//             System.out.println(ans);
+//             return;
+//         }
+//         for(int i=0;i<s.length();i++){
+//             uniquePermutation(s.substring(0, i)+s.substring(i+1),ans+s.charAt(i), list);
+//         }
+//     }
+// }
+
+
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+// public class Recursion_3 {
+//     public static void main(String[] args) {
+        
+//         String s="aba";
+//         uniquePermutation(s,"");
+//     }
+//     private static void uniquePermutation(String s,String ans){
+//         if(s.length()==0){
+//             System.out.println(ans);
+//             return;
+//         }
+//         for(int i=0;i<s.length();i++){
+//             boolean b=false;
+//             for(int j=i+1;j<s.length();j++){
+//                 if(s.charAt(i)==s.charAt(j)){
+//                     b=true;
+//                     break;
+//                 }
+//             }
+//             if(b==false){
+//                 uniquePermutation(s.substring(0, i)+s.substring(i+1),ans+s.charAt(i));
+//             }
+//         }
+//     }
+// }
+
+
+
+
+
+// leetcode 17
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recursion_3 {
+    
+    public static void main(String[] args) {
+        String arr[]={"","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        String digit="23";
+        for(int i=0;i<digit.length();i++){
+            String a=digit[i];
+            for(int j=digit[i];j<arr.length;j++){
+
+            }
+        }   
+    }
+}

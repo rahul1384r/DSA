357661e93105402ed6e142244b89ecebd850b8c1
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-02 11:12:14 +0530

    leetcode que_386

---
+++ leetcode_386.java
@@ -0,0 +1,21 @@
+public class leetcode_386{
+
+    public static void print(int n,int ans){
+        if(ans>n){
+            return;
+        }
+        System.out.println(ans);
+        int i=0;
+        if(ans==0){
+            i=1;
+        }
+        for(;i<=9;i++){
+            print(n,ans*10+i);
+        }
+    }
+    public static void main(String[] args) {
+        int n=15;
+        print(n,0);
+        
+    }
+}

cb25dce545448f96ce84990a487b3b0566787816
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-06 14:49:47 +0530

    2 que of backtracking

--- Backtracking_1.java
+++ Backtracking_1.java
@@ -1,14 +1,14 @@
 public class Backtracking_1 {
 
-    public static void print(int board[],int tq,int qp,String string){
+    public static void print(int board[],int tq,int qp,String string,int idx){
         if(qp>tq){
             System.out.println(string);
             return;
         }
-        for(int i=0;i<board.length;i++){
+        for(int i=idx;i<board.length;i++){
             if(board[i]==0){
                 board[i]=1;
-                print(board, tq, qp+1, string+"B"+i+"Q"+qp);
+                print(board, tq, qp+1, string+"B"+i+"Q"+qp,i+1);
                 board[i]=0;
             }
         }
@@ -16,6 +16,6 @@ public class Backtracking_1 {
     public static void main(String[] args) {
             int tq=2;
             int board[]=new int[4];
-            print(board,tq,1,"");
+            print(board,tq,1,"",0);
     }
 }
---
+++ backtracking_2.java
@@ -0,0 +1,12 @@
+public class backtracking_2 {
+
+    public static void print(int[] arr,int target,String ans){
+        if(target<0){
+            System.out.println(ans);
+            return;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}

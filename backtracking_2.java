9901d2259fa9d66c26a0a968396f6d17be355c53
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-06 15:24:56 +0530

    s

--- backtracking_2.java
+++ backtracking_2.java
@@ -1,9 +1,10 @@
 import java.util.*;
 public class backtracking_2 {
 
-    public static void print(int[] arr,int target,List<Integer> list){
+    public static void print(int[] arr,int target,List<Integer> list,List<Integer> ans){
         if(target==0){
             System.out.println(list);
+            ans.add(list);
             return;
         }
         if(target<0){
@@ -11,7 +12,7 @@ public class backtracking_2 {
         }
         for(int i=0;i<arr.length;i++){
             list.add(arr[i]);
-            print(arr,target-arr[i],list);
+            print(arr,target-arr[i],list,ans);
             list.remove(list.size()-1);
         }
     }
@@ -19,6 +20,7 @@ public class backtracking_2 {
         int arr[]={2,3,6,7};
         int target=7;
         List<Integer> list=new ArrayList<Integer>();
-        print(arr,7,list);
+        List<Integer> ans=new ArrayList<Integer>();
+        print(arr,7,list,ans);
     }
 }

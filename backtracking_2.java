b79565321dc39dc0b529583ec98947e6249f338c
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-07 16:29:22 +0530

    backtracking with arraylist

--- backtracking_2.java
+++ backtracking_2.java
@@ -4,7 +4,8 @@ public class backtracking_2 {
     public static void print(int[] arr,int target,List<Integer> list,List<List<Integer>> ans){
         if(target==0){
             System.out.println(list);
-            ans.add(list);
+            List<Integer> ll=new ArrayList<Integer>(list);
+            ans.add(ll);
             return;
         }
         if(target<0){

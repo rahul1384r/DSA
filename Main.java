2db7eff92cbb4bf371e3f83378f6751dc75a56c4
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-08 23:37:21 +0530

    s

--- Main.java
+++ Main.java
@@ -1,63 +1,50 @@
 import java.util.Scanner;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Input string
-        System.out.print("Enter a string: ");
-        String str = scanner.nextLine();
-        
-        // a.) Count 'hi'
-        int count = countHi(str);
-        System.out.println("Count of 'hi' (excluding 'hit'): " + count);
-        
-        // b.) Remove 'hi'
-        String removedHiString = removeHi(str);
-        System.out.println("String with 'hi' removed (excluding 'hit'): " + removedHiString);
-        
-        // c.) Replace 'hi' with 'bye'
-        String replacedHiString = replaceHi(str);
-        System.out.println("String with 'hi' replaced by 'bye' (excluding 'hit'): " + replacedHiString);
-    }
 
-    // a.) Recursive function to count 'hi'
-    public static int countHi(String str) {
-        if (str.length() < 2) {
-            return 0;
-        }
+        // Input number of test cases
+        System.out.print("Enter the number of test cases (T): ");
+        int T = scanner.nextInt();
 
-        if (str.startsWith("hi") && (str.length() == 2 || str.charAt(2) != 't')) {
-            return 1 + countHi(str.substring(2));
-        } else {
-            return countHi(str.substring(1));
+        for (int t = 0; t < T; t++) {
+            // Input size of the array
+            int N = scanner.nextInt();
+
+            // Input array elements
+            int[] arr = new int[N];
+            System.out.println("Enter the array elements for test case " + (t + 1) + ":");
+            for (int i = 0; i < N; i++) {
+                arr[i] = scanner.nextInt();
+            }
+
+            // Calculate and print the maximum number of points
+            int maxPoints = getMaxPoints(arr);
+            System.out.println("Maximum number of points for test case " + (t + 1) + ": " + maxPoints);
         }
     }
 
-    // b.) Recursive function to remove 'hi'
-    public static String removeHi(String str) {
-        if (str.length() < 2) {
-            return str;
+    // Function to calculate the maximum number of points
+    private static int getMaxPoints(int[] arr) {
+        int totalSum = 0;
+        for (int num : arr) {
+            totalSum += num;
         }
 
-        if (str.startsWith("hi") && (str.length() == 2 || str.charAt(2) != 't')) {
-            return removeHi(str.substring(2));
-        } else {
-            return str.charAt(0) + removeHi(str.substring(1));
-        }
-    }
+        int leftSum = 0;
+        int maxPoints = 0;
 
-    // c.) Recursive function to replace 'hi' with 'bye'
-    public static String replaceHi(String str) {
-        if (str.length() < 2) {
-            return str;
+        for (int i = 0; i < arr.length - 1; i++) {
+            leftSum += arr[i];
+            int rightSum = totalSum - leftSum;
+
+            if (leftSum == rightSum) {
+                maxPoints++;
+            }
         }
 
-        if (str.startsWith("hi") && (str.length() == 2 || str.charAt(2) != 't')) {
-            return "bye" + replaceHi(str.substring(2));
-        } else {
-            return str.charAt(0) + replaceHi(str.substring(1));
-        }
+        return maxPoints;
     }
 }

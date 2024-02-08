b980aecd75e894e964b9303f510771bdc4b582b2
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-08 22:00:02 +0530

    s

--- Main.java
+++ Main.java
@@ -1,19 +1,44 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        hanoi(N, 'A', 'B', 'C');
+
+        // Input: A single number
+        int number = scanner.nextInt();
+        ArrayList<String> result = getAllStrings(number, "");
+        Collections.sort(result);
+        for (String str : result) {
+            System.out.println(str);
+        }
     }
 
-    private static void hanoi(int N, char A, char B, char C) {
-        if (N == 1) {
-            System.out.println("Move 1 from " + A + " to " + B);
-            return;
+    private static ArrayList<String> getAllStrings(int number, String currentString) {
+        ArrayList<String> result = new ArrayList<>();
+
+        // Base case: If the number is 0, add the current string to the result
+        if (number == 0) {
+            result.add(currentString);
+            return result;
         }
-        hanoi(N - 1, A, C, B);
-        System.out.println("Move " + N + " from " + A + " to " + B);
-        hanoi(N - 1, C, B, A);
+
+        // Case 1: Take a single digit
+        int lastDigit = number % 10;
+        if (lastDigit != 0) {
+            char correspondingChar = (char) ('A' + lastDigit - 1);
+            result.addAll(getAllStrings(number / 10, correspondingChar + currentString));
+        }
+
+        // Case 2: Take two digits if applicable
+        if (number >= 10 && number % 100 <= 26) {
+            int lastTwoDigits = number % 100;
+            char correspondingChar = (char) ('A' + lastTwoDigits - 1);
+            result.addAll(getAllStrings(number / 100, correspondingChar + currentString));
+        }
+
+        return result;
     }
 }
--- check.java
+++ check.java
@@ -1,19 +1,77 @@
 import java.util.Scanner;
 
 public class check {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        hanoi(N, 'A', 'B', 'C');
+        int[][] grid = new int[N][N];
+        
+        // Input: Sample Sudoku puzzle
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = scanner.nextInt();
+            }
+        }
+
+        if (solveSudoku(grid)) {
+            printGrid(grid);
+        } else {
+            System.out.println("No solution exists for this Sudoku.");
+        }
     }
 
-    private static void hanoi(int N, char A, char B, char C) {
-        if (N == 1) {
-            System.out.println("Move 1 from " + A + " to " + B);
-            return;
+    // ... (rest of the code remains unchanged)
+    private static boolean solveSudoku(int[][] grid) {
+        int N = grid.length;
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col < N; col++) {
+                if (grid[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isSafe(grid, row, col, num)) {
+                            grid[row][col] = num;
+                            if (solveSudoku(grid)) {
+                                return true;
+                            }
+                            grid[row][col] = 0; // Backtrack
+                        }
+                    }
+                    return false;
+                }
+            }
         }
-        hanoi(N - 1, A, C, B);
-        System.out.println("Move " + N + " from " + A + " to " + B);
-        hanoi(N - 1, C, B, A);
+        return true;
+    }
+
+    private static boolean isSafe(int[][] grid, int row, int col, int num) {
+        int N = grid.length;
+        for (int i = 0; i < N; i++) {
+            if (grid[row][i] == num || grid[i][col] == num) {
+                return false;
+            }
+        }
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (grid[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static void printGrid(int[][] grid) {
+        int N = grid.length;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
+
+
+    

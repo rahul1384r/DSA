7df4efeff6aba7c561a48b487b565d3edf568bfd
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-08 22:15:56 +0530

    s

--- Main.java
+++ Main.java
@@ -1,44 +1,26 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input: A single number
-        int number = scanner.nextInt();
-        ArrayList<String> result = getAllStrings(number, "");
-        Collections.sort(result);
-        for (String str : result) {
-            System.out.println(str);
-        }
+        // Input: Enter a number in the form of a String
+        String str = scanner.nextLine();
+
+        // Convert the string to an integer using the recursive function
+        int result = stringToInteger(str);
+
+        // Print the result
+        System.out.println(result);
     }
 
-    private static ArrayList<String> getAllStrings(int number, String currentString) {
-        ArrayList<String> result = new ArrayList<>();
-
-        // Base case: If the number is 0, add the current string to the result
-        if (number == 0) {
-            result.add(currentString);
-            return result;
+    private static int stringToInteger(String str) {
+        if (str.isEmpty()) {
+            return 0;
         }
-
-        // Case 1: Take a single digit
-        int lastDigit = number % 10;
-        if (lastDigit != 0) {
-            char correspondingChar = (char) ('A' + lastDigit - 1);
-            result.addAll(getAllStrings(number / 10, correspondingChar + currentString));
-        }
-
-        // Case 2: Take two digits if applicable
-        if (number >= 10 && number % 100 <= 26) {
-            int lastTwoDigits = number % 100;
-            char correspondingChar = (char) ('A' + lastTwoDigits - 1);
-            result.addAll(getAllStrings(number / 100, correspondingChar + currentString));
-        }
-
-        return result;
+        int firstDigit = str.charAt(0) - '0';
+        int remainingDigits = stringToInteger(str.substring(1));
+        return firstDigit * (int) Math.pow(10, str.length() - 1) + remainingDigits;
     }
 }

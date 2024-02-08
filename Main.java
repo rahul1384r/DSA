65ffa20bd44ce0ae41068245fd45af616298e4b9
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-08 23:12:12 +0530

    s

--- Main.java
+++ Main.java
@@ -1,26 +1,63 @@
 import java.util.Scanner;
 
 public class Main {
-
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Input: Enter a number in the form of a String
+        
+        // Input string
+        System.out.print("Enter a string: ");
         String str = scanner.nextLine();
-
-        // Convert the string to an integer using the recursive function
-        int result = stringToInteger(str);
-
-        // Print the result
-        System.out.println(result);
+        
+        // a.) Count 'hi'
+        int count = countHi(str);
+        System.out.println("Count of 'hi' (excluding 'hit'): " + count);
+        
+        // b.) Remove 'hi'
+        String removedHiString = removeHi(str);
+        System.out.println("String with 'hi' removed (excluding 'hit'): " + removedHiString);
+        
+        // c.) Replace 'hi' with 'bye'
+        String replacedHiString = replaceHi(str);
+        System.out.println("String with 'hi' replaced by 'bye' (excluding 'hit'): " + replacedHiString);
     }
 
-    private static int stringToInteger(String str) {
-        if (str.isEmpty()) {
+    // a.) Recursive function to count 'hi'
+    public static int countHi(String str) {
+        if (str.length() < 2) {
             return 0;
         }
-        int firstDigit = str.charAt(0) - '0';
-        int remainingDigits = stringToInteger(str.substring(1));
-        return firstDigit * (int) Math.pow(10, str.length() - 1) + remainingDigits;
+
+        if (str.startsWith("hi") && (str.length() == 2 || str.charAt(2) != 't')) {
+            return 1 + countHi(str.substring(2));
+        } else {
+            return countHi(str.substring(1));
+        }
+    }
+
+    // b.) Recursive function to remove 'hi'
+    public static String removeHi(String str) {
+        if (str.length() < 2) {
+            return str;
+        }
+
+        if (str.startsWith("hi") && (str.length() == 2 || str.charAt(2) != 't')) {
+            return removeHi(str.substring(2));
+        } else {
+            return str.charAt(0) + removeHi(str.substring(1));
+        }
+    }
+
+    // c.) Recursive function to replace 'hi' with 'bye'
+    public static String replaceHi(String str) {
+        if (str.length() < 2) {
+            return str;
+        }
+
+        if (str.startsWith("hi") && (str.length() == 2 || str.charAt(2) != 't')) {
+            return "bye" + replaceHi(str.substring(2));
+        } else {
+            return str.charAt(0) + replaceHi(str.substring(1));
+        }
     }
 }

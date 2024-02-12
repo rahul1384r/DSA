405dce6ce205ed80e4739ffe8578014c612e4ade
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-12 17:28:07 +0530

    s

--- leetcode_79.java
+++ leetcode_79.java
@@ -4,17 +4,42 @@ public class leetcode_79 {
                       {'S','F','C','S'},
                       {'A','D','E','E'}};
         String word="ABCCED";
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<word.length();j++){
-                if(arr[i][j]==word.charAt(0)){
-                    search(arr,i,j,0);
+        for(int row=0;row<arr.length;row++){
+            for(int col=0;col<word.length();col++){
+                if(arr[row][col]==word.charAt(0)){
+                    boolean flag=search(arr,row,col,0,word);
+                    if(flag){
+                        System.out.println(true);
+                        return;
+                    }
                 }
             }
+            System.out.println(false);
         }
     }
 
-    private static void search(char[][] arr, int i, int j, int k) {
+    private static boolean search(char[][] arr, int row, int col, int idx,String word) {
+        if(idx==word.length()){
+            return true;
+        }
+        if(row<0 || col<0 || row==arr.length || col==arr[0].length || arr[row][col]!=word.charAt(idx)){
+            return false;
+        }
+
         int r[]={1,0,-1,0};
         int c[]={0,1,0,-1};
+
+        boolean flag=false;
+        char ch=arr[row][col];
+        arr[row][col]='*';
+        for(int i=0;i<4;i++){
+            flag=search(arr, row+r[i], col+c[i], idx+1,word);
+            if(flag){
+                return flag;
+            }
+        }
+        arr[row][col]=ch;
+        return flag;
+
     }
 }

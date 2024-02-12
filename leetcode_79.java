8fa0c6ab2d0375f8cacf827ae66c0e3e565fb560
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-12 16:34:39 +0530

    s

--- leetcode_79.java
+++ leetcode_79.java
@@ -1,5 +1,20 @@
 public class leetcode_79 {
     public static void main(String[] args) {
-        
+        char arr[][]={{'A','B','C','E'},
+                      {'S','F','C','S'},
+                      {'A','D','E','E'}};
+        String word="ABCCED";
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<word.length();j++){
+                if(arr[i][j]==word.charAt(0)){
+                    search(arr,i,j,0);
+                }
+            }
+        }
+    }
+
+    private static void search(char[][] arr, int i, int j, int k) {
+        int r[]={1,0,-1,0};
+        int c[]={0,1,0,-1};
     }
 }

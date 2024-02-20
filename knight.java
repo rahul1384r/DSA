4ba71d6f17f7d946b0ffeb912ef85f262870266f
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-20 14:46:14 +0530

    knight prblem

---
+++ knight.java
@@ -0,0 +1,13 @@
+public class knight {
+    public static void print(int[][] arr,int row,int col){
+        int r[]=2,1};
+        int c[]={1,2};
+        for(int i=0;i<c.length;i++){
+            print(arr,row+r[i],)
+        }
+    }
+    public static void main(String[] args) {
+        int arr[][]=new int[3][];
+        print(arr,0,0);
+    }
+}

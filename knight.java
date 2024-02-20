5dad6e5efd5d3b0ffd8f8b7126fd54de60ccd5df
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-20 15:46:18 +0530

    knight problem

--- knight.java
+++ knight.java
@@ -1,29 +1,43 @@
 public class knight {
-    public static void print(int[][] arr,int row,int col){
+    public static void print(int[][] arr,int row,int col,String ans){
         if(row>=arr.length || col>=arr[0].length){
             return;
         }
-        if(row==arr.length || col==arr[0].length){
+        ans+="{"+row+"-"+col+"}";
+        if(row== arr.length-1 && col==arr[0].length-1){
+            System.out.println(ans+" ");
+            count++;
             return;
         }
+        // for knight
         int r[]={2,1};
         int c[]={1,2};
         for(int i=0;i<c.length;i++){
-            print(arr,row+r[i],col+c[i]);
+            print(arr,row+r[i],col+c[i],ans+"K");
         }
+        // for rook
         if(row==0 || col==0 || row==arr.length-1 || col==arr[0].length-1){
+            // for row
             for(int i=1;i<arr.length;i++){
-                print(arr,row+i,col);
+                print(arr,row+i,col,ans+"R");
             }
+            // for col
             for(int i=1;i<arr[0].length;i++){
-                print(arr, row, col+i);
+                print(arr, row, col+i,ans+"R");
+            }
+        }
+        // for bishop
+        if(row==col || row+col==arr.length-1){
+            for(int i=1;i<arr.length;i++){
+                print(arr, row+i, col+i,ans+"B");
             }
         }
     }
-    
+    static int count=0;
     public static void main(String[] args) {
-        int arr[][]=new int[3][];
-        print(arr,0,0);
+        int arr[][]=new int[3][3];
+        print(arr,0,0,"");
+        System.out.println(count);
     }
 }
 

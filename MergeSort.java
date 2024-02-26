48fd233df4aafbc1b9484428de30c45ea10bec61
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-26 20:05:08 +0530

    mergesort by function

---
+++ MergeSort.java
@@ -0,0 +1,16 @@
+public class MergeSort {
+    public static void mergeSort(int[] arr1,int[] arr2){
+        int ans[]=new int[arr1.length+arr2.length];
+        int i=0;
+        int j=0;
+        int idx=0;
+        while(i<arr1.length && j<arr2.length){
+            
+        }
+    }
+    public static void main(String[] args) {
+        int arr1[]={1,2,4,7,8,9};
+        int arr2[]={3,5,6};
+        mergeSort(arr1,arr2);
+    }
+}

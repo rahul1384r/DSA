72b33c1d11d68a21513a4c660e00dc6e62d13496
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-26 20:07:51 +0530

    merge sort

--- MergeSort.java
+++ MergeSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MergeSort {
     public static void mergeSort(int[] arr1,int[] arr2){
         int ans[]=new int[arr1.length+arr2.length];
@@ -5,8 +7,20 @@ public class MergeSort {
         int j=0;
         int idx=0;
         while(i<arr1.length && j<arr2.length){
-            
+            if(arr1[i]<arr2[j]){
+                ans[idx++]=arr1[i++];
+            }
+            else{
+                ans[idx++]=arr2[j++];
+            }
         }
+        while(i<arr1.length){
+            ans[idx++]=arr1[i++];
+        }
+        while(j<arr2.length){
+            ans[idx++]=arr2[j++];
+        }
+        System.out.println(Arrays.toString(ans));
     }
     public static void main(String[] args) {
         int arr1[]={1,2,4,7,8,9};

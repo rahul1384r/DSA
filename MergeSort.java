dd3fa71fccc1aadd37b591ea8dee4b314b339d1b
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-03-11 16:46:27 +0530

    merge sort

--- MergeSort.java
+++ MergeSort.java
@@ -22,9 +22,20 @@ public class MergeSort {
         }
         System.out.println(Arrays.toString(ans));
     }
+    public static void mergeArray(int[] arr,int start,int end){
+        if(start==end){
+            return;
+        }
+        int mid=(start+end)/2;
+        mergeArray(arr, start, mid);
+        mergeArray(arr, mid+1, end);
+        mergeSort(arr, arr);
+    }
     public static void main(String[] args) {
         int arr1[]={1,2,4,7,8,9};
         int arr2[]={2,3,5,6};
         mergeSort(arr1,arr2);
     }
 }
+
+

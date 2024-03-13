71e68366eab73d69151f3f63cf517cbf3bbcb1f4
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-03-13 17:30:54 +0530

    addkth position

--- Linked List/LinkedList_1.java
+++ Linked List/LinkedList_1.java
@@ -26,6 +26,40 @@ public class LinkedList_1 {
         size++;
     }
 
+    public void addLast(int val){
+        Node nn=new Node();
+        nn.data=val;
+
+        if(size==0){
+            tail=nn;
+            head=nn;
+            nn.next=null;
+        }
+        else{
+            tail.next=nn;
+            nn=tail;
+        }
+        size++;
+    }
+
+    public void addKthPosition(int val,int k){
+        Node nn=new Node();
+        nn.data=val;
+        
+        Node kth=getNode(k);
+
+        Node temp=kth.next;
+        kth.next=nn;
+        nn.next=temp;
+    }
+
+    private Node getNode(int k){
+        Node temp=head;
+        for(int i=1;i<k;i++){
+            temp=temp.next;
+        }
+        return temp;
+    }
     public void display(){
         System.out.print("head"+"--->");
         Node temp=head;
@@ -41,6 +75,10 @@ public class LinkedList_1 {
         ll.addFirst(30);
         ll.addFirst(20);
         ll.addFirst(10);
+        ll.addLast(50);
+        ll.addLast(60);
+        ll.addLast(70);
+        ll.addKthPosition(72, 3);
         ll.display();
     }
 }

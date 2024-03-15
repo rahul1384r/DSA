30096f614f1cecd080a3fa21596281354fca638c
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-03-15 11:28:19 +0530

    Linked List

--- Linked List/LinkedList_1.java
+++ Linked List/LinkedList_1.java
@@ -9,6 +9,8 @@ public class LinkedList_1 {
     Node head;
     Node tail;
 
+
+    // function to add the element in the linked list at the first position of the linked list
     public void addFirst(int val){
         // object Node is created
         Node nn=new Node();
@@ -26,6 +28,7 @@ public class LinkedList_1 {
         size++;
     }
 
+    // finction to add the element in the linked list at the last position
     public void addLast(int val){
         Node nn=new Node();
         nn.data=val;
@@ -42,6 +45,7 @@ public class LinkedList_1 {
         size++;
     }
 
+    // function to add the element at the kth position of the linked list
     public void addKthPosition(int val,int k){
         Node nn=new Node();
         nn.data=val;
@@ -53,12 +57,59 @@ public class LinkedList_1 {
         nn.next=temp;
     }
 
+
     private Node getNode(int k){
         Node temp=head;
         for(int i=1;i<k;i++){
             temp=temp.next;
         }
         return temp;
+    }
+
+    // function to access the first element of the linked list
+    public int getFirst(){
+        return head.data;
+    }
+
+    // function to access the last element of the linked list
+    public int getLast(){
+        return tail.data;
+    }
+
+    // function to access the kth positioned element of the linked list
+    public int getKth(int k){
+        return getNode(k).data;
+    }
+
+    public void removeFirst(){
+        
+        Node temp= new Node();
+        temp=head;
+        // if(size==0){
+        //     head=null;
+        //     tail=null;
+        // }
+            head=temp.next;
+            temp.next=null;
+        size--;
+    }
+
+    
+    public void removeLast(){
+        Node temp=new Node();
+        temp=getNode(size-1);
+        temp.next=null;
+        tail=temp;
+        size--;
+        
+    }
+
+    public void removeKth(int k){
+        Node temp=getNode(k-1);
+        Node temp2=temp.next;
+        temp.next=temp.next.next;
+        temp2.next=null;
+        size--;
     }
     public void display(){
         System.out.print("head"+"--->");
@@ -67,18 +118,24 @@ public class LinkedList_1 {
             System.out.print(temp.data+"--->");
             temp=temp.next;
         }
-        System.out.print("null");
+        System.out.println("null");
     }
     public static void main(String[] args) {
         LinkedList_1 ll=new LinkedList_1();
+        ll.addFirst(50);
         ll.addFirst(40);
         ll.addFirst(30);
         ll.addFirst(20);
         ll.addFirst(10);
-        ll.addLast(50);
-        ll.addLast(60);
-        ll.addLast(70);
-        ll.addKthPosition(72, 3);
+        // ll.addLast(50);
+        // ll.addKthPosition(72, 3);
+        ll.display();
+        System.out.println(ll.getFirst());
+        System.out.println(ll.getLast());
+        System.out.println(ll.getKth(2));
+        ll.removeFirst();
+        ll.removeLast();
+        ll.removeKth(2);
         ll.display();
     }
 }
